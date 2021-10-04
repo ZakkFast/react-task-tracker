90c41bf38ab18f9fa425e971ba1eb092a91a081d
Zakk Fast <zakkfastpro@gmail.com>
2021-10-04 11:28:19 -0700

    added social media links;

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,13 @@
+import { SocialIcon } from "react-social-icons"
+
+const SocialMedia = () => {
+    return (
+        <div className='social-media'>
+            <SocialIcon url='https://github.com/ZakkFast' />
+            <SocialIcon url='https://www.linkedin.com/in/zachary-fast/' />
+            <SocialIcon url='mailto:webdev@zakkfast.com' />
+        </div>
+    )
+}
+
+export default SocialMedia
