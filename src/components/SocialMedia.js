0b0577f37817572022c5a9451f9d8287e768c500
Zakk Fast <zakkfastpro@gmail.com>
2021-10-04 11:40:23 -0700

    set link target to _blank

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,9 +3,9 @@ import { SocialIcon } from "react-social-icons";
 const SocialMedia = () => {
   return (
     <div className="social-media">
-      <SocialIcon url="https://github.com/ZakkFast" />
-      <SocialIcon url="https://www.linkedin.com/in/zachary-fast/" />
-      <SocialIcon url="mailto:webdev@zakkfast.com" />
+      <SocialIcon url="https://github.com/ZakkFast" target='_blank' />
+      <SocialIcon url="https://www.linkedin.com/in/zachary-fast/" target='_blank' />
+      <SocialIcon url="mailto:webdev@zakkfast.com" target='_blank' />
     </div>
   );
 };
