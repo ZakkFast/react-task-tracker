5925d3615f31fcd93a290c40f31a2cc61d1c4e12
Zakk Fast <zakkfastpro@gmail.com>
2021-10-04 11:27:34 -0700

    added about sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import SocialMedia from "./components/SocialMedia";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -49,15 +53,25 @@ function App() {
   };
 
   return (
+    <Router>
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
+      
+      <Route path='/' exact render={(props) => (
+        <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
+        </>
+      )} />
+      <Route path='/about' component={About}/>
+      <Footer />
     </div>
+    <SocialMedia />
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+const About = () => {
+    return (
+        <div className='task text-center'>
+            <p>This is a Single Page app made with React</p>
+            <p>Click Add to create a task.</p>
+            <p>Double Click a Task to set reminder</p>
+            <p>Click the red 'x' to delete a Task</p>
+           <h4>Version 1.0.2</h4>
+        </div>
+    )
+}
+
+export default About
